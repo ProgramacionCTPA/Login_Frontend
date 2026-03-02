3abb0cbdc5835f279ddf73e7c3a2ecdfeb61b32f
ProgramacionCTPA <160040652+ProgramacionCTPA@users.noreply.github.com>
2026-03-01 18:53:35 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const API = "http://localhost:3000";
+
+async function register() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch(`${API}/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+    alert(data.message);
+    if(res.ok) window.location.href = "index.html";
+}
+
+async function login() {
+    const username = document.getElementById("loginUsername").value;
+    const password = document.getElementById("loginPassword").value;
+
+    const res = await fetch(`${API}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+
+    if(res.ok) {
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("username", data.username);
+        window.location.href = "menu.html";
+    } else {
+        alert(data.message);
+    }
+}
+
+function logout() {
+    localStorage.clear();
+    window.location.href = "index.html";
+}
+
+if(window.location.pathname.includes("menu.html")) {
+    const username = localStorage.getItem("username");
+    if(!username) {
+        window.location.href = "index.html";
+    } else {
+        document.getElementById("user").textContent = username;
+    }
+}
